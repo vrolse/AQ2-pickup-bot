8c2f252fb844ef70137cd6a221f0a8315e324be3
vrolse <101935079+vrolse@users.noreply.github.com>
2023-07-18 00:23:45 +0200

    Update sendtoAPI-test.lua

--- lua/sendtoAPI-test.lua
+++ lua/sendtoAPI-test.lua
@@ -20,11 +20,10 @@ function LogMessage(msg)
         -- Get player data
         local playerData = {}
         for i, plr in pairs(ex.players) do
-            if plr.name ~= "[MVDSPEC]" then
-                local frags = ex.ClientStats(i, STAT_FRAGS)
-                table.insert(playerData, { name = plr.name, score = frags })
-            end
+            local frags = ex.ClientStats(i, STAT_FRAGS)
+            table.insert(playerData, { name = plr.name, score = frags })
         end
+
 
         -- Function to convert a Lua value to its JSON representation
         local function jsonEncode(value)
