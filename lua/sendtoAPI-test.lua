99555d0a32fe534b87916e8ca99331017d94568f
vrolse <101935079+vrolse@users.noreply.github.com>
2023-07-19 09:39:03 +0200

    Update sendtoAPI-test.lua

--- lua/sendtoAPI-test.lua
+++ lua/sendtoAPI-test.lua
@@ -1,10 +1,31 @@
+-- vrol's attempt on lua-scriptning.. 
+-- Test to send map result to AQ2-pickup bot "API"
+
 local APIaddr = "http://test.server.api:1234/" -- change this
 local team1 = nil
 local team2 = nil
 
 function runWithTimeout(command, timeout)
     local timeoutCommand = string.format("timeout %d %s", timeout, command)
-    return os.execute(timeoutCommand)
+    local handle = io.popen(timeoutCommand, "r")
+    local output = handle:read("*a")
+    handle:close()
+
+    return output
+end
+
+function escapeString(str)
+    return str:gsub('[%z\1-\31\128-\255"\'\\]', {
+        ['\a'] = '\\a',
+        ['\b'] = '\\b',
+        ['\f'] = '\\f',
+        ['\n'] = '\\n',
+        ['\r'] = '\\r',
+        ['\t'] = '\\t',
+        ['\v'] = '\\v',
+        ['"'] = '\\"',
+        ['\\'] = '\\\\',
+    })
 end
 
 function LogMessage(msg)
@@ -21,51 +42,28 @@ function LogMessage(msg)
         local playerData = {}
         for i, plr in pairs(ex.players) do
             local frags = ex.ClientStats(i, STAT_FRAGS)
-            table.insert(playerData, { name = plr.name, score = frags })
-        end
-
-
-        -- Function to convert a Lua value to its JSON representation
-        local function jsonEncode(value)
-            if type(value) == "string" then
-                return '"' .. value:gsub('[%c"\\]', {
-                    ['\t'] = '\\t',
-                    ['\n'] = '\\n',
-                    ['\r'] = '\\r',
-                    ['"'] = '\\"',
-                    ['\\'] = '\\\\',
-                }) .. '"'
-            elseif type(value) == "table" then
-                local parts = {}
-                for k, v in pairs(value) do
-                    table.insert(parts, jsonEncode(k) .. ":" .. jsonEncode(v))
-                end
-                return "{" .. table.concat(parts, ",") .. "}"
-            else
-                return tostring(value)
-            end
+            table.insert(playerData, { name = escapeString(plr.name), score = frags })
         end
 
         -- Convert the 'playerData' table to a JSON string
         local playerJsonStrings = {}
         for _, player in ipairs(playerData) do
-            table.insert(playerJsonStrings, jsonEncode(player))
+            local jsonStr = string.format('{"name":"%s","score":%d}', player.name, player.score)
+            table.insert(playerJsonStrings, jsonStr)
         end
 
         -- Join player JSON strings into an array
         local playerJsonArray = "[" .. table.concat(playerJsonStrings, ",") .. "]"
 
         local jsonPayload = string.format('{"ID":"%s","name":"%s","T1":"%s","T2":"%s","map":"%s","players":%s}', fname, hostname, team1, team2, map, playerJsonArray)
-        local cmd = string.format("curl -X PUT -H 'Content-Type: application/json' -d '%s' %s", jsonPayload, APIaddr)
+        local cmd = string.format("curl -X PUT -H \"Content-Type: application/json\" -d %q %s", jsonPayload, APIaddr)
 
-        local exitCode = runWithTimeout(cmd, 1)  -- Timeout set to 1 second so server does not freeze for too long
+        local output = runWithTimeout(cmd, 1)
 
-        if exitCode == 124 then
-            print('Timed out')
-        elseif exitCode == 0 then
-            print('Match score sent!')
+        if output == nil then
+            print('Error executing the command')
         else
-            print('Error code:', exitCode)
+            print('Match score sent!')
         end
 
         return true
